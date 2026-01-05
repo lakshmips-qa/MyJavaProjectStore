d338e895145a7bad72a1ecbd2a671e5f1ec6ed5e
lakshmips-qa <lakshmipstech2022@gmail.com>
2026-01-05 14:58:39 +0530

    Datatype & Variables

---
+++ src/JavaBasics/DataVar.java
@@ -0,0 +1,20 @@
+package JavaBasics;
+
+public class DataVar {
+    static void main() {
+        byte b=10;
+        short s=20;
+        int i=30;
+        char c='A';
+        float f=12.8f;
+        double d=13456.8;
+        String name="Lakshmi";
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(name);
+    }
+}
