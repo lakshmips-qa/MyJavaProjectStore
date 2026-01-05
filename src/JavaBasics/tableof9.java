740c8ce054428415fc6d83fb4d6c788d9eb8b321
lakshmips-qa <lakshmipstech2022@gmail.com>
2026-01-05 15:18:30 +0530

    table of 9

---
+++ src/JavaBasics/tableof9.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+
+public class tableof9 {
+    static void main() {
+        int n = 9;
+
+        System.out.printf("%d x 1 = %d%n", n, n * 1);
+        System.out.printf("%d x 2 = %d%n", n, n * 2);
+        System.out.printf("%d x 3 = %d%n", n, n * 3);
+        System.out.printf("%d x 4 = %d%n", n, n * 4);
+        System.out.printf("%d x 5 = %d%n", n, n * 5);
+        System.out.printf("%d x 6 = %d%n", n, n * 6);
+        System.out.printf("%d x 7 = %d%n", n, n * 7);
+        System.out.printf("%d x 8 = %d%n", n, n * 8);
+        System.out.printf("%d x 9 = %d%n", n, n * 9);
+        System.out.printf("%d x 10 = %d%n", n, n * 10);
+    }
+}
